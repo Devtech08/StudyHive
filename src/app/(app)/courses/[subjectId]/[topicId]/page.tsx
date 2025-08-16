d8d6000ed95870f5072bc0544b39da31b67d4193
Prince <dadaprince011@gmail.com>
2025-08-16 13:34:17 +0000

    the changes, didnt work

--- src/app/(app)/courses/[subjectId]/[topicId]/page.tsx
+++ src/app/(app)/courses/[subjectId]/[topicId]/page.tsx
@@ -1,6 +1,6 @@
 import { notFound } from 'next/navigation';
 import Link from 'next/link';
-import { getTopic } from '@/lib/data';
+import { getTopic, getSubject } from '@/lib/data';
 import {
   Breadcrumb,
   BreadcrumbItem,
